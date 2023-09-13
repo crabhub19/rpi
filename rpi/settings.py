d7c7fae395d824215461c4ac787b7f28e981cecf
Bishal <144455368+crabhub19@users.noreply.github.com>
2023-09-13 18:19:08 +0600

    Update settings.py

--- rpi/settings.py
+++ rpi/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f_v@bha0c^1o+g-pukc99a77p3*1s7chp+cs)^&+&e$b+7akt6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
